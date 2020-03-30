f2193e6e39ed88832135a4165bb7192114b1cd47
Adrian <1718111017@utectulancingo.edu.mx>
2020-03-29 20:06:00 -0600

    Ahora si

--- application/controllers/alumnos.py
+++ application/controllers/alumnos.py
@@ -7,11 +7,12 @@ render = web.template.render('application/controllers/')   #En esta no se ocupa
 
 class Alumnos:
     def GET(self):
-        #try:
         datos = web.input() 
         if datos['token']=="1234":
             result=[]
             result2={}
+
+            #Obtiene todo el contenido del archivo alumnos.csv
             if datos['action']=="get":
                 with open('static/csv/alumnos.csv') as csvfile:
                     reader = csv.DictReader(csvfile)
@@ -21,36 +22,46 @@ class Alumnos:
                         result2['appVersion']="1.0.1"
                         result2['alumnos']=result
                 return json.dumps(result2)
-            elif datos['action']=="filter":
-                filtro = datos['filtro']
+
+            #Obtiene solo la fila que coincida el campo matricula
+            elif datos['action']=="search":
+                with open('static/csv/alumnos.csv','r') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    result = []
+                    for row in reader:
+                        if str(row['matricula'])==datos['matricula']:
+                            result.append(row)
+                            result2['status']="200 OK"
+                            result2['appVersion']="1.0.1"
+                            result2['alumnos']=result
+                return json.dumps(result2)
+
+            #Inserta una nueva fila en el archivo alumnos.csv
+            elif datos["action"] == "put":
+                m1 = datos["matricula"]
+                m2 = datos["nombre"]
+                m3 = datos["primer_apellido"]
+                m4 = datos["segundo_apellido"]
+                m5 = datos["carrera"]
+                result = []
+                result.append(m1)
+                result.append(m2)
+                result.append(m3)
+                result.append(m4)
+                result.append(m5)
+                with open ('static/csv/alumnos.csv','a+', newline = '') as csvfiles:
+                    writer = csv.writer(csvfiles)
+                    writer.writerow(result)
                 with open('static/csv/alumnos.csv') as csvfile:
                     reader = csv.DictReader(csvfile)
                     for row in reader:
-                        result.append(row[filtro])
+                        result.append(row)
                         result2['status']="200 OK"
                         result2['appVersion']="1.0.1"
                         result2['alumnos']=result
                 return json.dumps(result2)
-            elif datos['action'] == "search":
-                inf = {}
-                inf['app_version']="0.2.0"
-                inf['status']="200 OK"
-                matricula = str(datos['matricula'])
-                result="matricula,nombre,primer_apellido,segundo_apellido,carrera\n"
-                with open('static/csv/alumnos.csv', 'r') as csvfile:
-                    reader = csv.reader(csvfile)
-                    result=[]
-                    for row in reader:
-                        if row[0]==matricula:
-                            resulta={}
-                            resulta['matricula']=str(row['matricula'])
-                            resulta['nombre']=str(row['nombre'])
-                            resulta['primer_apellido']=str(row['primer_apellido'])
-                            resulta['segundo_apellido']=str(row['segundo_apellido'])
-                            resulta['carrera']=str(row['carrera'])
-                            result.append(resulta)
-                        inf['alumnos']=result
-                return json.dumps(inf)
+
+            #Elimina la fila completa donde coincida la matricula especificada        
             elif datos['action']=="delete":
                 inf = {}
                 inf['app_version']="0.2.0"
@@ -67,8 +78,30 @@ class Alumnos:
                 with open('static/csv/salida.csv', 'r') as inp, open('static/csv/alumnos.csv', 'w') as out:
                     writer = csv.writer(out)
                     for row in csv.reader(inp):
-                        writer.writerow(row)  
+                        writer.writerow(row)
+                with open('static/csv/alumnos.csv') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        result.append(row)
+                        result2['status']="200 OK"
+                        result2['appVersion']="1.0.1"
+                        result2['alumnos']=result
+                return json.dumps(result2)  
                 print("Si se pudo burro")
+
+            #Este lo que hace es que se le da la columna que quiere que se le retorne
+            #ejemplo: http://localhost:8080/alumnos?action=filter&filtro=matricula&token=1234
+            #Esto da como resultado que solo se muestren las matriculas, y así con las demás columnas
+            elif datos['action']=="filter":
+                filtro = datos['filtro']
+                with open('static/csv/alumnos.csv') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        result.append(row[filtro])
+                        result2['status']="200 OK"
+                        result2['appVersion']="1.0.1"
+                        result2['alumnos']=result
+                return json.dumps(result2)
             else:
                 result2={}
                 result2['status']="Command not found get"
@@ -77,8 +110,3 @@ class Alumnos:
             result={}
             result['status']="Los datos insertados son incorrectos"
             return json.dumps(result)
-        """except Exception as e:
-            result={}
-            text= "ups algo paso{}".format(e.args)
-            result  ['status'] = text 
-            return json.dumps(result)"""
